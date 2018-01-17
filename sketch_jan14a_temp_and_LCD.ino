880e391d4d00c5fe77fb77c926c3cd185ad96c35
vandorb12 <32138616+vandorb12@users.noreply.github.com>
2018-01-16 23:13:09 -0800

    Basic LCD and temperature code working.
    No running average yet.

---
+++ sketch_jan14a_temp_and_LCD.ino
@@ -0,0 +1,60 @@
+
+
+/*
+  Temperature and humidity average sensor with LCD output
+
+  Written by Thomas Avent
+
+  Using the input libaries of a DHT11 v2 and the output libraries of the most common LCD driver,
+  a printed average of the temperature and humidity will present itself over 30 minute averages.
+ */
+
+// Libraires to include
+#include <LiquidCrystal.h> // LCD library
+#include <dht.h> // DHT11 v2 library
+dht DHT;
+
+// Declare constants
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+const int DHT11_PIN = 9; // Pin 9 is used for DHT11 v2
+
+// C to F function for temperature conversion
+int tempF(int tempC)
+{
+  int output = (tempC * 1.8) + 32;
+  return output;
+}
+
+void setup() 
+ {
+    // Prepare serial communications
+    Serial.begin(9600);
+    // setup the LCD's number of columns and rows
+    lcd.begin(16,2);
+    // Print a welcome message to the LCD and to serial
+//    lcd.print("Hello, World!");
+//    Serial.println("Hello, World!");
+//    // Delay 5 seconds before clearing LCD
+//    delay(5000);
+//    lcd.clear(); // Clears LCD
+//    delay(500);
+    //lcd.setCursor(0,0); // Implied after lcd.clear(), but handy to have.
+    
+     
+ }
+
+void loop() 
+  {
+    // put your main code here, to run repeatedly:
+    DHT.read11(DHT11_PIN); // Function to read the temperature and humidity into memory
+    lcd.setCursor(0,0);
+    lcd.print("Current Temp:");
+    lcd.setCursor(0,1); // Sets LCD cursor to column 0, line 1 (first column, line 2)
+    lcd.print(tempF(DHT.temperature)); // Prints temperature in F
+    Serial.print(tempF(DHT.temperature), 1);
+    Serial.print("\n");
+    lcd.blink();
+    delay(2000); // DHT11 doesn't like to make fast reads on sensor data. A delay of 2 seconds tends to keep it from erring out.
+
+}
